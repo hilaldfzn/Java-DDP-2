65686448aa8a0bbb7af4290222d807f883c18240
Muhammad Hilal Darul Fauzan <121853767+hilaldfzn@users.noreply.github.com>
2023-06-19 19:13:54 +0700

    TP03 Done

---
+++ TP 03 - CuciCuci III: CuciCuci Cuci/assignment3/user/menu/EmployeeSystem.java
@@ -0,0 +1,89 @@
+package assignments.assignment3.user.menu;
+
+import assignments.assignment3.nota.Nota;
+import assignments.assignment3.user.Employee;
+import assignments.assignment3.user.Member;
+
+import static assignments.assignment3.nota.NotaManager.notaList;
+
+public class EmployeeSystem extends SystemCLI {
+
+    /**
+     * Membuat object baru EmployeeSystem dan mendaftarkan Employee pada CuciCuci
+     */
+    public EmployeeSystem() {
+        memberList = new Member[] {
+                new Employee("Dek Depe", "akuDDP"),
+                new Employee("Depram", "musiktualembut"),
+                new Employee("Lita Duo", "gitCommitPush"),
+                new Employee("Ivan Hoshimachi", "SuamiSahSuisei"),
+        };
+    }
+
+    public void addEmployee(Employee[] employees) {
+        Member[] result = new Member[employees.length + memberList.length];
+    
+        System.arraycopy(memberList, 0, result, 0, memberList.length);
+        System.arraycopy(employees, 0, result, memberList.length, employees.length);
+        memberList = result;
+    }
+
+    /**
+     * Memproses pilihan dari employee yang masuk ke sistem ini sesuai dengan menu specific.
+     *
+     * @param choice -> pilihan pengguna.
+     * @return true jika user log.
+     */
+    @Override
+    protected boolean processChoice(int choice) {
+        boolean logout = false;
+        switch (choice) {
+            case 1 -> processNyuciTime();
+            case 2 -> displayListNota();
+            case 3 -> logout = true;
+            default -> System.out.println("Pilihan tidak valid, silakan coba lagi.\n");
+        }
+        return logout;
+    }
+
+    /**
+     * Displays specific menu untuk Employee.
+     */
+    @Override
+    protected void displaySpecificMenu() {
+        System.out.println("1. It's nyuci time");
+        System.out.println("2. Display List Nota");
+        System.out.println("3. Logout");
+    }
+ 
+    protected void processNyuciTime() {
+        if (notaList.length != 0) {
+            System.out.printf("Stand back! %s beginning to nyuci!%n", loginMember.getNama());
+            
+            /* Iterasi semua nota yang ada pada sistem dan mencetak
+             * proses apa yang sedang dijalankan pada semua nota
+             */
+            for (Nota nota : notaList) {                                                      
+                System.out.printf("Nota %d : %s%n", nota.getId(), nota.kerjakan());    
+                nota.setNotaStatus();
+            }
+            System.out.println();
+        } else {
+            System.out.println("Belum ada nota di dalam sistem!\n");     // Jika belum ada nota yang dibuat
+        }
+    }
+
+    protected void displayListNota() {
+        if (notaList.length != 0) {
+            /* Iterasi semua nota yang ada pada sistem dan mencetak
+             * status semua nota (Sudah selesai/Belum selesai)
+             */
+            for (Nota nota : notaList) {
+                System.out.printf("Nota %d : %s%n", nota.getId(), nota.getNotaStatus());
+            }
+            System.out.println();
+        } else {
+            System.out.println("Belum ada nota di dalam sistem!\n");     // Jika belum ada nota yang dibuat
+        }
+    }
+}
