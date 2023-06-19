3f2c7a01d8496d78374344da966001f6afa14156
Muhammad Hilal Darul Fauzan <121853767+hilaldfzn@users.noreply.github.com>
2023-06-19 19:11:24 +0700

    TP03 Done

---
+++ TP 03 - CuciCuci III: CuciCuci Cuci/assignment3/nota/NotaManager.java
@@ -0,0 +1,37 @@
+package assignments.assignment3.nota;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class NotaManager {
+    public static SimpleDateFormat fmt = new SimpleDateFormat("dd/MM/yyyy");
+    public static Calendar cal = Calendar.getInstance();
+    static public Nota[] notaList = new Nota[0];
+
+    /**
+     * Skips ke hari berikutnya dan update semua entri nota yang sesuai.
+     */
+    public static void toNextDay() {
+        cal.add(Calendar.DATE, 1);      
+
+        for (Nota nota : notaList) {          
+            nota.toNextDay();
+        }
+    }
+
+    /**
+     * Menambahkan nota baru ke NotaList.
+     *
+     * @param nota Nota object untuk ditambahkan.
+     */
+    public static void addNota(Nota nota) {
+        Nota[] tempNotaList = new Nota[notaList.length + 1];
+
+        for (int i = 0; i < notaList.length; i++) {
+            tempNotaList[i] = notaList[i];
+        }
+
+        notaList = tempNotaList;
+        tempNotaList[notaList.length - 1] = nota;
+    }
+}
